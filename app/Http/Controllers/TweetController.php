d32e433cdd61912b69adec54c5bdb5aefbbb6ef3
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2022-01-16 11:44:31 +0630

    single tweet api done

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -8,6 +8,10 @@ class TweetController extends Controller
 {
     public function index()
     {
-        return Tweet::with('user:id,name,username,avatar,created_at')->latest()->get();
+        return Tweet::with('user:id,name,username,avatar,created_at')->latest()->paginate(10);
+    }
+    public function show(Tweet $tweet)
+    {
+        return $tweet->load('user:id,name,username,avatar,created_at');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/tweets', [TweetController::class,'index']);
+Route::get('/tweets/{tweet}', [TweetController::class,'show']);
